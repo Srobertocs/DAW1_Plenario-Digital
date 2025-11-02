7450cc081814ee257f45e9199074f21bc003937e
Silvio Roberto <sousasilvio441@gmail.com>
2025-11-01 22:43:13 -0300

    Merge branch 'main' of https://github.com/Srobertocs/DAW1_Plenario-Digital

---
+++ plenariodigital/src/main/java/com/plenariodigital/plenariodigital/PlenariodigitalApplication.java
@@ -0,0 +1,13 @@
+package com.plenariodigital.plenariodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlenariodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlenariodigitalApplication.class, args);
+	}
+
+}
---
+++ plenariodigital/src/test/java/com/plenariodigital/plenariodigital/PlenariodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plenariodigital.plenariodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlenariodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
