3e6da6a8ab597a906e8764ce38efbc3d85e0f815
Silvio Roberto <sousasilvio441@gmail.com>
2025-11-25 12:46:13 -0300

    Inicio do desenvolvimento da página de cadastro dos partidos

--- plenariodigital/src/main/java/com/plenariodigital/controller/PartidoController.java
+++ plenariodigital/src/main/java/com/plenariodigital/controller/PartidoController.java
@@ -2,13 +2,19 @@ package com.plenariodigital.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.plenariodigital.model.Partido;
 
 @Controller
 public class PartidoController {
-  @GetMapping("/partido/cadastro_partido.html")
-  public String abrirCadastro(Partido partido) {
-    return "/partido/cadastro_partido.html";
+  @GetMapping("/partido/cadastro_partido")
+  public String abrirCadastro(Partido partido,
+      @RequestHeader(value = "HX-Request", required = false) String hxRequestHeader) {
+    if (hxRequestHeader != null) {
+      return "partido/cadastro_partido :: formulario";
+    } else {
+      return "partido/cadastro_partido";
+    }
   }
 }
